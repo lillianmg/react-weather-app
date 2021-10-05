b2c54cc217600c31c237cec942dc1036f2d97c97
Lillian <lillianmarlee@gmail.com>
2021-10-04 22:06:31 -0500

    Added Axios integration
    Now shows the weather of a selected city

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from "react";
+import Weather from "./Weather";
+import './index.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+export default function App() {
+  return ( 
+  <div className="App">
+    <div className="container"></div>
+ <Weather defaultCity="New York" />
+<footer>Weather App created by Lillian Gil and is{" "}<a
+ href="https://github.com/lillianmg"
+ target="_blank"
+>
+open-sourced on GitHub
+</a>
+   </footer>
+ </div>
   );
-}
-
-export default App;
+} 
